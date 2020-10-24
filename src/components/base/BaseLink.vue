c9c45ec716ac706436ec2882290afcf8096fa9be
saloev <saloev.saadi@yandex.ru>
2020-10-25 01:57:26 +0300

    feat: add base linkt component

---
+++ src/components/base/BaseLink.vue
@@ -0,0 +1,95 @@
+<script lang="ts">
+import router from "@/router";
+/* eslint-disable */
+import { VNode, CreateElement } from "vue";
+import { Vue, Prop, Component } from "vue-property-decorator";
+
+@Component({})
+export default class BaseLink extends Vue {
+  @Prop({
+    type: String,
+    default: "router-link",
+    validator(val) {
+      return val === "router-link" || val === "a";
+    }
+  })
+  type?: string;
+
+  @Prop({
+    default: "/"
+  })
+  link!: Object | string;
+
+  @Prop({
+    type: Boolean,
+    default: false
+  })
+  flex?: boolean;
+
+  @Prop({
+    type: Boolean,
+    default: false
+  })
+  justifyCenter?: boolean;
+
+  @Prop({
+    type: Boolean,
+    default: false
+  })
+  alignCenter?: boolean;
+
+  @Prop({
+    type: String,
+    default: "#343030"
+  })
+  color?: string;
+
+  public render(h: CreateElement): VNode {
+    return h(
+      this.type,
+      {
+        class: {
+          "base-link": true,
+          "base-link--flex": this.flex,
+          "base-link--justify-center": this.justifyCenter,
+          "base-link--align-center": this.alignCenter
+        },
+        props: {
+          to: this.link
+        },
+        style: {
+          color: this.color
+        }
+      },
+      [this.$slots.default]
+    );
+  }
+}
+</script>
+<style lang="scss">
+.base-link {
+  &--flex {
+    display: flex;
+  }
+  &--justify-center {
+    justify-content: center;
+  }
+  &--align-center {
+    align-items: center;
+  }
+
+  font-size: 14px;
+  line-height: 150%;
+  text-decoration: none;
+
+  @include transition();
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  &:focus {
+    outline: 2px dashed;
+  }
+}
+</style>
--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,12 +1,31 @@
 <template>
-  <div class="main-layout"></div>
+  <div class="main-layout">
+    <MainHeader class="main-layout__header" />
+
+    <main class="main-layout__content">
+      <router-view />
+    </main>
+
+    <MainFooter class="main-layout__footer" />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
+import MainHeader from "@/components/layout/MainHeader.vue";
+import MainFooter from "@/components/layout/MainFooter.vue";
+
 @Component({
-  components: {},
+  components: {
+    MainHeader,
+    MainFooter,
+  },
 })
 export default class MainLayout extends Vue {}
 </script>
+
+<style lang="scss">
+.main-layout {
+}
+</style>
